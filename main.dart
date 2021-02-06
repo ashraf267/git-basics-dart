b30da9032831048a559f61bc5c222fe02e91069a
Ashraf Otagun <Otagunolaniyi7@gmail.com>
2021-02-06 00:52:25 -0800

    wrote Hello world in dart

---
+++ main.dart
@@ -0,0 +1,16 @@
+// Lesson-1
+// This is a tutorial for learning basics of git using dart programming language
+// This tutorial assumes you already have git and git bash installed
+// And you already know the basics of dart
+// What have I done here?
+// I created a remote repo and clone it into my local computer
+// -using the git cmd: git clone repo-link
+// Then open the local repo folder with vscode
+// And created a file: main.dart, using the cmd: touch main.dart
+// Then, I created a branch called 'lesson-1', using the git cmd: git branch lesson-1
+// -and checkout to that branch using git cmd: git checkout lesson-1
+// tips: to check all git branches, use: git branch -a
+
+void main() {
+  print('Hello world');
+}
